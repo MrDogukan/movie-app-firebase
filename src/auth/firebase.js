fc3fab2bd8ff41e74bdc2341163270d90509f811
aliseslikaya <aliseslikaya@gmail.com>
2022-07-27 03:37:02 +0300

    bugs fixed

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,79 +1,94 @@
 import { initializeApp } from "firebase/app";
-import { getAuth,createUserWithEmailAndPassword,signInWithEmailAndPassword,onAuthStateChanged,signOut, updateProfile,signInWithPopup,GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 import { toastErrorNotify, toastSuccessNotify } from "../helpers/toastify";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-
 // Initialize Firebase Authentication and get a reference to the service
 
 const auth = getAuth(app);
-export const createUser = async (email,password,navigate,displayName)=>{
-    try{
-      let userCredential=await createUserWithEmailAndPassword(auth, email, password);
-      await updateProfile(auth.currentUser,{
-        displayName: displayName
-      })
-      toastSuccessNotify("Registered successfully")
-      navigate("/")
-      sessionStorage.setItem("user",JSON.stringify(userCredential.user))
-      
-    }catch(error){
-        toastErrorNotify(error.message);
-    }
-}
+export const createUser = async (email, password, navigate, displayName) => {
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    await updateProfile(auth.currentUser, {
+      displayName: displayName,
+    });
+    toastSuccessNotify("Registered successfully");
+    navigate("/");
+    sessionStorage.setItem("user", JSON.stringify(userCredential.user));
+  } catch (error) {
+    toastErrorNotify(error.message);
+  }
+};
 
 //to sign in
-export const signIn = async(email,password,navigate)=>{
-    
-    try{
-        let userCredential=await signInWithEmailAndPassword(auth, email, password)
-        navigate("/")
-        toastSuccessNotify("Logged in successfully")
-      }catch(error){
-        toastErrorNotify(error.message);
-      }
-}
+export const signIn = async (email, password, navigate) => {
+  try {
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+    toastSuccessNotify("Logged in successfully");
+    console.log(userCredential);
+  } catch (error) {
+    toastErrorNotify(error.message);
+  }
+};
 
 // to prevent double register
-export const userObserver = (setCurrentUser)=>{
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            setCurrentUser(user)
-        } else {
-            setCurrentUser(false)
-        }
-      });
-    
-}
+export const userObserver = (setCurrentUser) => {
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setCurrentUser(user);
+    } else {
+      setCurrentUser(false);
+    }
+  });
+};
 
 //to log out
-export const logout = ()=>{
-    signOut(auth)
-    toastSuccessNotify("Logged out successfully")
-}
+export const logout = () => {
+  signOut(auth);
+  toastSuccessNotify("Logged out successfully");
+};
 
 //while deploying add on vercel,netlify etc. add that link to firebase google authorized domains list
-export const signUpProvider = (navigate)=>{
-    const provider = new GoogleAuthProvider();
+export const signUpProvider = (navigate) => {
+  const provider = new GoogleAuthProvider();
 
-signInWithPopup(auth, provider)
-  .then((result) => {
-    console.log(result);
-    navigate("/")
-  }).catch((error) => {
-    console.log(error);
-  });
-}
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      console.log(result);
+      navigate("/");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,68 +1,79 @@
-import { useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import axios from "axios"
-import { useState } from 'react';
-import { toastErrorNotify } from '../helpers/toastify';
+import { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+import { toastErrorNotify } from "../helpers/toastify";
 const MovieDetail = () => {
-  
-  const [details,setDetails]=useState({})
-  const {id}=useParams()
-  const {title,poster_path,overview,release_date,vote_average,vote_count}=details
+  const [details, setDetails] = useState({});
+  const { id } = useParams();
+  const {
+    title,
+    poster_path,
+    overview,
+    release_date,
+    vote_average,
+    vote_count,
+  } = details;
   const API_KEY = process.env.REACT_APP_TMDB_KEY;
   const movieDetailBaseUrl = `https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`;
   const baseImageUrl = "https://image.tmdb.org/t/p/w1280";
   const defaultImage =
     "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-    const fetchData = async() =>{
-      try {
-        const {data}=await axios.get(movieDetailBaseUrl)
-        setDetails(data)
-      } 
-      catch (error) {
-        toastErrorNotify(error.message)
-      }}
+  const fetchData = async () => {
+    try {
+      const { data } = await axios.get(movieDetailBaseUrl);
+      setDetails(data);
+    } catch (error) {
+      toastErrorNotify(error.message);
+    }
+  };
 
-    useEffect(() => {
-      fetchData()
-    }, [movieDetailBaseUrl])
-    
-    // console.log(details);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // console.log(details);
 
   return (
     <div className="container py-5">
-    <h1 className="text-center fw-bold">{title}</h1>
-    <div className="card mb-3">
-      <div className="row g-0">
-        <div className="col-md-4 detailImg">
-          <img
-            src={poster_path ? baseImageUrl + poster_path : defaultImage}
-            className="img-fluid rounded-start"
-            alt="..."
-          />
-        </div>
-        <div className="col-md-8 d-flex flex-column ">
-          <div className="card-body">
-            <h5 className="card-title fw-bold text-center">Overview</h5>
-            <p className="card-text">{overview}</p>
+      <h1 className="text-center fw-bold">{title}</h1>
+      <div className="card mb-3">
+        <div className="row g-0">
+          <div className="col-md-4 detailImg">
+            <img
+              src={poster_path ? baseImageUrl + poster_path : defaultImage}
+              className="img-fluid rounded-start"
+              alt="..."
+            />
           </div>
-          <ul className="list-group ">
-            <li className="list-group-item">
-              <span className='fw-bold'>Release Date :</span> {release_date}
-            </li>
-            <li className="list-group-item"> <span className='fw-bold'>Rate :</span>{vote_average}</li>
-            <li className="list-group-item">
-             <span className='fw-bold'>Total Vote :</span>{vote_count}
-            </li>
-            <li className="list-group-item" >
-              <Link to={-1}> Go Back</Link>
-            </li>
-          </ul>
+          <div className="col-md-8 d-flex flex-column ">
+            <div className="card-body">
+              <h5 className="card-title fw-bold text-center">Overview</h5>
+              <p className="card-text">{overview}</p>
+            </div>
+            <ul className="list-group ">
+              <li className="list-group-item">
+                <span className="fw-bold">Release Date :</span> {release_date}
+              </li>
+              <li className="list-group-item">
+                {" "}
+                <span className="fw-bold">Rate :</span>
+                {vote_average}
+              </li>
+              <li className="list-group-item">
+                <span className="fw-bold">Total Vote :</span>
+                {vote_count}
+              </li>
+              <li className="list-group-item">
+                <Link to={-1}> Go Back</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-)
-}
+  );
+};
 
-export default MovieDetail
+export default MovieDetail;
